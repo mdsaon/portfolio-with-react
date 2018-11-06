db941e4208db1c0b4e93bfb1f87ce5364f1ef08a
saidur rahman <swesaon@gmail.com>
2018-11-06 15:24:18 +0200

    projects

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React from "react";
+import card from "../../Images/card.png";
+import "./stylesheet.css";
 
 const Projects = () => {
-    return (
-        <div className="content">Hello Projects page</div>
-    )
-}
+  return (
+    <div className="content-projects">
+      <h3>My Projects</h3>
+      <div className="projects-wrapper">
+        <div className="projects">
+          <img src={card} alt="Img" width="200px" height="200px"/>
+          <div className="projects-info">
+            <p>
+              <b>Developed by using React,Redux,Nodejs and MongoDb </b>
+            </p>
+          </div>
+        </div>
+        <div className="projects">
+        <img src={card} alt="Img" width="200px" height="200px"/>
+          <div className="projects-info">
+            <p>
+              <b>Developed by using React,Redux,Nodejs and MongoDb </b>
+            </p>
+          </div>
+        </div>
+        <div className="projects">
+        <img src={card} alt="Img" width="200px" height="200px"/>
+          <div className="projects-info">
+            <p>
+              <b>Developed by using React,Redux,Nodejs and MongoDb </b>
+            </p>
+          </div>
+        </div>
+        <div className="projects">
+        <img src={card} alt="Img" width="200px" height="200px"/>
+          <div className="projects-info">
+            <p>
+              <b>Developed by using React,Redux,Nodejs and MongoDb </b>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
