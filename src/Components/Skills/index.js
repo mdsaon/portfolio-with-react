25dadc661cdc492d1f7421de44b9a68c6d7a2db8
saidur rahman <swesaon@gmail.com>
2018-11-06 13:37:20 +0200

    skills

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import "./stylesheet.css";
 
 const Skills = () => {
-    return (
-        <div className="content">Hello Skills page</div>
-    )
-}
+  return (
+    <div className="content-skills">
+      <h3>My Skills</h3>
+        <div className="skills-wrapper">
+         <div className="skills">Html
+            <div className="skills html">80%</div>
+         </div>
+         <div className="skills">Css
+         <div className="skills css">80%</div>
+         </div>
+         <div className="skills">JavaScript
+         <div className="skills js">80%</div>
+         </div>
+         <div className="skills">Php
+         <div className="skills php">80%</div>
+         </div>
+         <div className="skills">MySql
+         <div className="skills mysql">80%</div>
+         </div>
+         <div className="skills">MongoDb
+         <div className="skills mongodb">80%</div>
+         </div>
+         <div className="skills">Sql
+         <div className="skills sql">80%</div>
+         </div>
+         <div className="skills">NodeJs
+         <div className="skills nodejs">80%</div>
+         </div>
+        </div>
+    </div>
+  );
+};
 
 export default Skills;
